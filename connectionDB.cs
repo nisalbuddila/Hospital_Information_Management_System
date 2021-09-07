5c2bbbcfbc3d235b0775bc0e268d9038d548ea91
Nisal Gamage <nisalbuddila@gmail.com>
2021-09-07 19:21:08 +0530

    Database Connection Class Created

--- connectionDB.cs
+++ connectionDB.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data.SqlClient;
+using System.Data.Sql;
 
 namespace Hospital_Information_Management_System
 {
-    class connectionDB
+    public class connectionDB
     {
+        public SqlConnection connectDB()
+        {
+            string server = "";
+            string database = "HIMS";
+            string uid = "";
+            string password = "";
+            string conString = "Data Source=" + server + ";Initial Catalog=" + database + ";User ID=" + uid + ";Password=" + password + "";
+            SqlConnection con = new SqlConnection(conString);
+            return con;
+        }
     }
 }
