b88850f9c7afd3520a51ab982d24db1e9de5d820
ShadiSDJ <shaadijayathilaka@gmail.com>
2021-09-22 12:53:40 +0530

    Medical Officer UI & Profile

---
+++ MOuserDetails.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hospital_Information_Management_System
+{
+    public partial class MOuserDetails : Form
+    {
+        public MOuserDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void lblPersonal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
